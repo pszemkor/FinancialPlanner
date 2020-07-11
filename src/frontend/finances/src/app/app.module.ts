7ff889f4b8ab5692b5b2dd976e07f912ad678b57
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-07-11 16:38:16 +0200

    Create basic header

--- src/frontend/finances/src/app/app.module.ts
+++ src/frontend/finances/src/app/app.module.ts
@@ -4,16 +4,30 @@ import {RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {HeaderComponent} from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    RouterModule
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
