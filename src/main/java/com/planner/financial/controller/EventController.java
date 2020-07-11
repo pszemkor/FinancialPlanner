5bf066fd2b638be1b83876db787a2e9eef2ac39c
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-07-11 13:37:43 +0200

    Implement simple structure of backend

--- src/main/java/com/planner/financial/controller/EventController.java
+++ src/main/java/com/planner/financial/controller/EventController.java
@@ -1,12 +1,29 @@
 package com.planner.financial.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.planner.financial.model.Event;
+import com.planner.financial.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/events")
 public class EventController {
-    private final
-    @GetMapping("/")
+    private final EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public List<Event> getAll() {
+        return eventService.getAllEvents();
+    }
+
+    @PostMapping
+    public void insertEvent(@RequestBody Event event) {
+        this.eventService.insertEvent(event);
+    }
 }
--- src/main/java/com/planner/financial/service/EventService.java
+++ src/main/java/com/planner/financial/service/EventService.java
@@ -1,19 +1,26 @@
 package com.planner.financial.service;
 
+import com.planner.financial.model.Event;
 import com.planner.financial.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EventService {
-    private final EventRepository repository;
+    private final EventRepository eventRepository;
 
     @Autowired
-    public EventService(EventRepository repository) {
-        this.repository = repository;
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
     }
 
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
 
-
-
+    public void insertEvent(Event event) {
+        this.eventRepository.save(event);
+    }
 }
