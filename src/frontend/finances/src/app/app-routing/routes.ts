34221a58f5cbac42d357b75a1ac94ab259f6a9c9
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-07-11 18:03:49 +0200

    Add finances component to header

--- src/frontend/finances/src/app/app-routing/routes.ts
+++ src/frontend/finances/src/app/app-routing/routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router'
 
 import { HomeComponent } from '../home/home.component';
+import {FinancesComponent} from "../finances/finances.component";
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'finances', component: FinancesComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
