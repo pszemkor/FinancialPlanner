5b5e42878095ccd5832a3454a7e16ca3fce6aa8f
Przemysław Jabłecki <prz.jab98@gmail.com>
2020-07-14 20:47:34 +0200

    Add unit tests for EventService

--- src/main/java/com/planner/financial/model/Event.java
+++ src/main/java/com/planner/financial/model/Event.java
@@ -1,5 +1,6 @@
 package com.planner.financial.model;
 
+import com.google.common.base.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,11 +13,15 @@ public class Event {
     private Date date;
     private EventType type;
     private Double value;
+    private String name;
+    private String description;
 
-    public Event(Date date, EventType type, Double value) {
+    public Event(Date date, EventType type, Double value, String name, String description) {
         this.date = date;
         this.type = type;
         this.value = value;
+        this.name = name;
+        this.description = description;
     }
 
     public Event() {
@@ -36,5 +41,31 @@ public class Event {
 
     public Double getValue() {
         return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equal(id, event.id) &&
+                Objects.equal(date, event.date) &&
+                type == event.type &&
+                Objects.equal(value, event.value) &&
+                Objects.equal(name, event.name) &&
+                Objects.equal(description, event.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, date, type, value, name, description);
     }
 }
---
+++ src/test/java/com/planner/financial/service/EventServiceTest.java
@@ -0,0 +1,90 @@
+package com.planner.financial.service;
+
+import com.google.common.collect.Iterables;
+import com.planner.financial.model.Event;
+import com.planner.financial.model.EventType;
+import com.planner.financial.repository.EventRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+class EventServiceTest {
+    EventRepository eventRepository;
+
+    @BeforeEach
+    void setUp() {
+        eventRepository = mock(EventRepository.class);
+        List<Event> events = new LinkedList<>();
+        Date testDate = new Date(2020, Calendar.SEPTEMBER, 15);
+
+        Event event1 = new Event(testDate, EventType.INCOME, 1.0, "someName1", "");
+        events.add(event1);
+        Event event2 = new Event(new Date(2020, Calendar.OCTOBER, 12), EventType.EXPENDITURE, 10.0, "someName2", "");
+        events.add(event2);
+        Event event3 = new Event(new Date(2021, Calendar.MARCH, 1), EventType.EXPENDITURE, 2.0, "someName3", "");
+        events.add(event3);
+        Event event4 = new Event(new Date(2020, Calendar.OCTOBER, 11), EventType.INCOME, 200.0, "someName4", "");
+        events.add(event4);
+
+        when(eventRepository.findAll()).thenReturn(events);
+        when(eventRepository.getEventsByMonthAndYear(testDate)).thenReturn(Collections.singletonList(event1));
+        when(eventRepository.getEventsByYear(testDate)).thenReturn(Arrays.asList(event1, event2, event4));
+        when(eventRepository.getTotalValueByDate(testDate)).thenCallRealMethod();
+    }
+
+    @Test
+    void shouldReturnAllEventsWhenRequested() {
+        EventService eventService = new EventService(eventRepository);
+
+        List<Event> allEvents = eventService.getAllEvents();
+        Set<String> eventNames = allEvents.stream()
+                .map(Event::getName)
+                .collect(Collectors.toSet());
+
+        assertEquals(4, allEvents.size());
+        assertThat(eventNames).containsOnly("someName1", "someName2", "someName3", "someName4");
+    }
+
+    @Test
+    void shouldCallSaveMethod() {
+        EventService eventService = new EventService(eventRepository);
+        Event newEvent = new Event(new Date(0), EventType.INCOME, 100.0, "new", "new one!");
+
+        eventService.insertEvent(newEvent);
+
+        verify(eventRepository, times(1)).save(newEvent);
+    }
+
+    @Test
+    void getTotalValueByDate() {
+        EventService eventService = new EventService(eventRepository);
+        Date referenceDate = new Date(2020, Calendar.SEPTEMBER, 15);
+
+        Map<String, Double> totalValueByDate = eventService.getTotalValueByDate(referenceDate);
+
+        assertEquals(190.0, totalValueByDate.get("OCTOBER"));
+        assertEquals(1.0, totalValueByDate.get("SEPTEMBER"));
+        assertEquals(0.0, totalValueByDate.get("JANUARY"));
+
+    }
+
+    @Test
+    void shouldGetAllEventsByDate() {
+        EventService eventService = new EventService(eventRepository);
+        Date referenceDate = new Date(2020, Calendar.SEPTEMBER, 15);
+
+        List<Event> allEventsByDate = eventService.getAllEventsByDate(referenceDate);
+
+        assertEquals(1, allEventsByDate.size());
+        assertEquals(referenceDate, Iterables.getOnlyElement(allEventsByDate).getDate());
+    }
+
+}
