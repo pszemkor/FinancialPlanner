30ed5113a04a36c54b6db422999b9bb77184d55b
okarinbro <prz.jab98@gmail.com>
2020-08-30 18:19:21 +0200

    Add buttons for year manipulation

--- src/frontend/finances/src/app/finances/finances.component.ts
+++ src/frontend/finances/src/app/finances/finances.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EventsService} from "../services/events.service";
 import {monthNames} from "../shared/months";
+import {SynchronizationService} from "../services/synchronization.service";
 
 @Component({
   selector: 'app-finances',
@@ -12,12 +13,27 @@ export class FinancesComponent implements OnInit {
   monthNames = monthNames;
   balance: any;
 
-  constructor(private eventsService: EventsService) {
+  constructor(private eventsService: EventsService, private synchronizationService :SynchronizationService) {
     this.currentYear = new Date().getFullYear();
   }
 
   ngOnInit(): void {
+    this.getEvents();
+    this.synchronizationService.updatedEvents.subscribe(_ => this.getEvents())
+  }
+
+  private getEvents() {
     this.eventsService.retrieveMonthBalance(String(this.currentYear))
       .subscribe(events => this.balance = events, error => console.log(error));
+  }
+
+  onPreviousMonth() {
+    this.currentYear --;
+    this.getEvents();
+  }
+
+  onNextMonth(){
+    this.currentYear ++;
+    this.getEvents();
   }
 }
